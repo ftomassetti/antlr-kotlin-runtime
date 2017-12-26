3312ec1f63502cb8ecc36b2770cff8aa67bb686b
Federico Tomassetti <federico@tomassetti.me>
2017-12-26 19:31:00 +0100

    improving the generation

--- antlr-kotlin-runtime-jvm/src/main/kotlin/org/antlr/v4/kotlinruntime/facade/Parser.kt
+++ antlr-kotlin-runtime-jvm/src/main/kotlin/org/antlr/v4/kotlinruntime/facade/Parser.kt
@@ -1,23 +1,20 @@
 package org.antlr.v4.kotlinruntime.facade
 
-import org.antlr.v4.runtime.Parser
-
-actual abstract class Parser {
+actual abstract class Parser : org.antlr.v4.runtime.Parser{
     private val _grammarFileName: String
-    actual constructor(grammarFileName: String, input: TokenStream) {
-        this.wrapped = Parser(input)
+    actual constructor(grammarFileName: String, input: TokenStream) : super(input.wrapped) {
         this._grammarFileName = grammarFileName
         if (true) TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     protected var _ctx: ParserRuleContext? = null
 
-    open fun getGrammarFileName() : String = _grammarFileName
+    override open fun getGrammarFileName() : String = _grammarFileName
 
     open fun enterRule(localctx: ParserRuleContext?, state: Int, ruleIndex: Int) {
         TODO("not implemented")
         //super.enterRule(localctx.wrapped, state, ruleIndex)
     }
 
-    val wrapped: Parser
+
 }
