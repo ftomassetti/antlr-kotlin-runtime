d5fb5df5f9a60b85cc343d26099869f34664f017
Federico Tomassetti <federico@tomassetti.me>
2018-01-03 19:18:41 +0100

    testing toCharArray

--- antlr-kotlin-runtime-js/src/main/kotlin/com/strumenta/kotlinmultiplatform/toCharArray.kt
+++ antlr-kotlin-runtime-js/src/main/kotlin/com/strumenta/kotlinmultiplatform/toCharArray.kt
@@ -1,7 +1,8 @@
 package com.strumenta.kotlinmultiplatform
 
 actual fun String.toCharArray(): CharArray {
-
-    return this.split("").map { it[0] }.toCharArray()
-    //TODO("String.toCharArray not implemented") //To change body of created functions use File | Settings | File Templates.
+    val res = CharArray(this.length) {
+        this[it]
+    }
+    return res
 }
