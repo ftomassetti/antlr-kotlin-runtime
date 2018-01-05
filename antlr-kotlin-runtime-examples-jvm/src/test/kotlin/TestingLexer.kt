8f3ac0053322624a4fdfbf6135764eea5a287667
Federico Tomassetti <federico@tomassetti.me>
2018-01-05 21:46:08 +0100

    adding more tests

--- antlr-kotlin-runtime-examples-jvm/src/test/kotlin/TestingLexer.kt
+++ antlr-kotlin-runtime-examples-jvm/src/test/kotlin/TestingLexer.kt
@@ -1,4 +1,7 @@
 import org.antlr.v4.kotlinruntime.ANTLRInputStream
+import org.antlr.v4.kotlinruntime.atn.EmptyPredictionContext
+import org.antlr.v4.kotlinruntime.atn.PredictionContext
+import org.antlr.v4.kotlinruntime.atn.PredictionContextCache
 import org.junit.Test as test
 import org.junit.Assert.*
 
@@ -12,6 +15,15 @@ class TestingLexer {
         assertEquals(11, result)
     }
 
+    @test fun checkMaxs() {
+        assertEquals(2147483647, PredictionContext.EMPTY_RETURN_STATE)
+    }
+
+    @test fun emptyPredictionContext() {
+        assertEquals(true, EmptyPredictionContext().hasEmptyPath())
+        assertEquals(true, EmptyPredictionContext().isEmpty)
+    }
+
     @test fun firstTokenDebug2() {
         val input = ANTLRInputStream("1 + 2")
         val lexer = MiniCalcLexer(input)
@@ -22,20 +34,6 @@ class TestingLexer {
         assertEquals(true, dfa!!.s0 != null)
         assertEquals(0, dfa!!.s0!!.stateNumber)
     }
-
-//    @test fun firstTokenDebug3() {
-//        val input = ANTLRInputStream("1 + 2")
-//        val lexer = MiniCalcLexer(input)
-//        val interpreter = lexer.interpreter
-//        val atn = interpreter!!.atn
-//        val mode = 0
-//        val startState = atn.modeToStartState[mode]
-//        val s0_closure = interpreter!!.computeStartState(input, startState)
-//        assertEquals(true, s0_closure.uniqueAlt)
-//        assertEquals(0, dfa!!.s0!!.stateNumber)
-//    }
-//
-//
 
     @test fun simpleTokens() {
         val lexer = MiniCalcLexer(ANTLRInputStream("1 + 2"))
